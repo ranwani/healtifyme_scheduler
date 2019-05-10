e99b757130ed40f9b859d8f1fbdeb5767cdc7d0c
ranwani <rahul.anwani@gmail.com>
2019-05-11 01:43:01 +0530

    Update the unit test case constants

--- scheduler/constants.py
+++ scheduler/constants.py
@@ -125,31 +125,35 @@ OUTPUT_4 = [
                 {"start_time": "2017-01-05 09:00:00", "end_time": "2017-01-05 10:00:00"}
 ]
 
-#  Failed Validation Constants
-
-#  Case 1: start_time key missing from Monday's dict
-NUM_5 = 10
-DATE_5 = datetime(2017, 1, 1, 20, 30)
+#  Constants with n = 4 and a different date
+NUM_5 = 4
+DATE_5 = datetime(2019, 5, 4, 20, 30)  # Saturday
 INPUT_5 = [
             [  # Monday
-                {"": "06:00", "end_time": "06:30"},
-                {"start_time": "06:30", "end_time": "07:00"},
+                {"start_time": "06:00", "end_time": "06:30"},
             ],
             [  # Tuesday
             ],
             [  # Wednesday
             ],
             [  # Thursday
-                {"start_time": "09:00", "end_time": "10:00"}
             ],
             [  # Friday
             ],
             [  # Saturday
+                {"start_time": "09:00", "end_time": "10:00"}
             ],
             [  # Sunday
             ]
 ]
-EXCEPTION_TYPE_5 = TypeError
+OUTPUT_5 = [
+                {"start_time": "2019-05-06 06:00:00", "end_time": "2019-05-06 06:30:00"},
+                {"start_time": "2019-05-11 09:00:00", "end_time": "2019-05-11 10:00:00"},
+                {"start_time": "2019-05-13 06:00:00", "end_time": "2019-05-13 06:30:00"},
+                {"start_time": "2019-05-18 09:00:00", "end_time": "2019-05-18 10:00:00"}
+]
+
+#  Failed Validation Constants
 
 #  Case 2: Length of week_config is not 7
 NUM_6 = 10
@@ -207,6 +211,30 @@ INPUT_9 = [
 ]
 EXCEPTION_TYPE_9 = ValueError
 
+#  Case 6: start_time key missing from Monday's dict
+NUM_10 = 10
+DATE_10 = datetime(2017, 1, 1, 20, 30)
+INPUT_10 = [
+            [  # Monday
+                {"": "06:00", "end_time": "06:30"},
+                {"start_time": "06:30", "end_time": "07:00"},
+            ],
+            [  # Tuesday
+            ],
+            [  # Wednesday
+            ],
+            [  # Thursday
+                {"start_time": "09:00", "end_time": "10:00"}
+            ],
+            [  # Friday
+            ],
+            [  # Saturday
+            ],
+            [  # Sunday
+            ]
+]
+EXCEPTION_TYPE_10 = TypeError
+
 
 SAMPLE_INPUT_OUTPUTS = [
     (DATE_1, INPUT_1, OUTPUT_1),
@@ -215,13 +243,14 @@ SAMPLE_INPUT_OUTPUTS = [
 ]
 
 SAMPLE_INPUT_OUTPUTS_WITH_N = [
-    (DATE_4, INPUT_4, NUM_4, OUTPUT_4)
+    (DATE_4, INPUT_4, NUM_4, OUTPUT_4),
+    (DATE_5, INPUT_5, NUM_5, OUTPUT_5)
 ]
 
 FAILED_VALIDATION_INPUT_OUTPUTS = [
-    (DATE_5, INPUT_5, NUM_5, EXCEPTION_TYPE_5),
     (DATE_6, INPUT_6, NUM_6, EXCEPTION_TYPE_6),
     (DATE_7, INPUT_7, NUM_7, EXCEPTION_TYPE_7),
     (DATE_8, INPUT_8, NUM_8, EXCEPTION_TYPE_8),
-    (DATE_9, INPUT_9, NUM_9, EXCEPTION_TYPE_9)
+    (DATE_9, INPUT_9, NUM_9, EXCEPTION_TYPE_9),
+    (DATE_10, INPUT_10, NUM_10, EXCEPTION_TYPE_10),
 ]
